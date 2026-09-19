3fc5e7f444a890d41d4d0906a6ba690d2448d5a1
bhaskarsachan32-beep <bhaskarsachan32@gmail.com>
2026-09-19 06:44:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-
     const header = req.headers.authorization;
 
-     if (!header || !header.startsWith("Bearer ")) {
-  return res.status(401).json({
-    message: "Authentication required"
-  });
-}
+    if (!header || !header.startsWith("Bearer ")) {
+        return res.status(401).json({
+            message: "Authentication required"
+        });
+    }
 
     const token = header.split(" ")[1];
 
     try {
-
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        );
-
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
-
         next();
-
     } catch (error) {
-
         return res.status(401).json({
             message: "Invalid or expired token"
         });
